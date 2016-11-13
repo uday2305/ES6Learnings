f36a0143e35e92a4b1bf05a79bd9f853ec6d0914
uday2305 <udaybhaskar2305@gmail.com>
2016-11-13 18:22:39 +0100

    promise - ex1 - initial commit

---
+++ Promise/utils.js
@@ -0,0 +1,39 @@
+function get(url) {
+  // Return a new promise.
+  // We do all the work within the constructor callback.
+  var requestPromise = new Promise(function(resolve, reject) {
+    // Do the usual XHR stuff
+    var req = new XMLHttpRequest();
+    req.open('get', url);
+
+    req.onload = function() {
+      // 'load' triggers for 404s etc
+      // so check the status
+      if (req.status == 200) {
+        // Resolve the promise with the response text
+        resolve(req.response);
+      }
+      else {
+        // Otherwise reject with the status text
+        reject(Error(req.statusText));
+      }
+    };
+
+    // Handle network errors
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    // Make the request
+    req.send();
+  });
+
+  return requestPromise.then(function(results) {
+    return results[1];
+  });
+}
+
+
+function getJson(url) {
+  return get(url).then(JSON.parse);
+}
